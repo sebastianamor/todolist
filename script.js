6058c5548efd92e24b9847fdf1f4fa8766351e50
¨Jane <smoreira11@hotmail.com>
2024-01-09 22:43:53 +0900

    作り中！！

--- script.js
+++ script.js
@@ -4,12 +4,30 @@ const dateMonth  = document.getElementById("dateMonth");
 const dateYear   = document.getElementById("dateYear");
 
 //Tasks Container
-const tasksContainer = document.getElementById("taskContainer");
+const tasksContainer = document.getElementById("tasksContainer");
 
 const setDate = () => {
     const date = new Date();
-    dateNumber.textContent = date.toLocaleString('jp', {day:"numeric"});
-    dateText.textContent = date.toLocaleString('jp',{weekday:'long'});
-    dateMonth.textContent = date.toLocaleString('jp',{month:"short"});
-    dateYear.textContent = date.toLocaleString("jp", {year:"numeric"});
+    dateNumber.textContent = date.toLocaleString('jp', { day:"numeric"});
+    dateText.textContent = date.toLocaleString('jp',{ weekday:'long'});
+    dateMonth.textContent = date.toLocaleString('jp',{ month:"short"});
+    dateYear.textContent = date.toLocaleString("jp", { year:"numeric"});
+};
+
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText; 
+    if(!value) return;
+    const task = document.createElement("div");
+    task.classList.add("task","roundBorder");
+    task.addEventListener("click", changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
 }
+
+setDate();
